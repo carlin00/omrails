440e16df524ddd96957951590ea472c5ec1352bb
carlin00 <alejanse@gmail.com>
2023-03-03 19:30:32 +0100

    Aktivert bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about' => 'pages#about'
   get 'contact' => 'pages#contact'
+  get 'faq' => 'pages#faq'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
